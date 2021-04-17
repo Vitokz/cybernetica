3ed196de6420b8376a790452b2655f1e93ac005e
Amiran <chicobes1@gmail.com>
2021-04-17 11:17:18 +0300

    Готовый парсер CSV

--- CSVTask/internal/answerCheck.go
+++ CSVTask/internal/answerCheck.go
@@ -1,8 +1,7 @@
 package internal
 
 import (
-	//"fmt"
-
+	"fmt"
 	"log"
 	"math/rand"
 	"strconv"
@@ -13,10 +12,10 @@ func Check(userAns, rightAns string) (string, bool) {
 	rand.Seed(time.Now().UnixNano())
 	right := []string{"Правильно!", "Лучший", "Повезло Повезло", "Бог математики"}
 	incorrect := []string{"Ну почти", "Неправильно", "даже Сосик из 7б решил", "Пифагор осуждает"}
-
 	usAns, err := strconv.Atoi(userAns)
 	if err != nil {
-		log.Fatal(err.Error())
+		fmt.Println("Буквы вводить запрещено")
+		return incorrect[rand.Intn(4)], false
 	}
 	riAns, _ := strconv.Atoi(rightAns)
 	if err != nil {
--- CSVTask/main.go
+++ CSVTask/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"Vitokz/CSVTask/internal"
+	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-
-	csvFile:=flag.String("file","csvdata/problems.csv","файл с тестами") //Флаг задающий имя файла который нужно спрасить и провести тест
-    seconds:=flag.Int("timer",30,"Время на ответ")
+	//  Флаги
+	/*Флаг для вставки файла*/
+	csvFile := flag.String("file", "csvdata/problems.csv", "файл с тестами") //Флаг задающий имя файла который нужно спрасить и провести тест
+	/*флаг изменения времени*/
+	seconds := flag.Int("timer", 30, "Время на ответ")
+	/*Парсинг флагов*/
 	flag.Parse()
+	//
+
 	//Открытие файла CSV
 	file, err := os.Open(*csvFile)
 	//Проверка открылся ли фаил
@@ -24,34 +32,47 @@ func main() {
 	//Close закрывает фаил
 	defer file.Close()
 
-	reader := csv.NewReader(file)        //NewReader  читает фаил и создает класс который читает данные из file
-	lencsv, _ := reader.Read()         //Достаем первую строку для определения длинны строки
-	reader.FieldsPerRecord = len(lencsv) //берем элементы до числа len(lencsv)
+	reader := csv.NewReader(file) //NewReader  читает фаил и создает класс который читает данные из file
+	reader.FieldsPerRecord = 2    //берем элементы до 2 каждой строки
 
+	//Массив для сбора информации о правильных и неправильных ответах
 	var arrayAns = map[string]int{
 		"right":     0,
 		"incorrect": 0,
 	}
-	_ = arrayAns
+
+	/* Канал */
+	chanel := make(chan string)
+	/* Таймер */ timer := time.NewTimer(time.Duration(*seconds) * time.Second)
+loop: // "Имя" функции
 	for {
 		record, e := reader.Read() //Достаю строки поочередно
-		if e != nil {
-			fmt.Println(e)
+		if e != nil {              //Проверка досталась ли строка
 			break
 		}
-        
-		fmt.Println(record[0] + "=")
-		fmt.Printf("У вас %d с. Введите ответ:", *seconds)
 
-		answer := internal.Input(*seconds)
+		fmt.Print(record[0] + "=")
+		fmt.Print("Введите ответ:")
 
-		check,ok:=internal.Check(strings.TrimSpace(answer),record[1])
-		fmt.Println(check + "\n")
-		if ok {
-			arrayAns["right"]++
-		}else{
-			arrayAns["incorrect"]++
+		go func() { //Горутина для ввода ответов
+			reader := bufio.NewReader(os.Stdin)
+			inp, _ := reader.ReadString('\n')
+			chanel <- inp
+		}()
+
+		select {
+		case <-timer.C: //Кейс проверяющий не остановился ли таймер
+			break loop
+		case answer := <-chanel: //Засчитывание ответов
+			check, ok := internal.Check(strings.TrimSpace(answer), record[1]) //Функция проверки ответа
+			fmt.Println(check + "\n")
+			if ok {
+				arrayAns["right"]++
+			} else {
+				arrayAns["incorrect"]++
+			}
 		}
 	}
-	fmt.Printf("Правильных ответов:%d Неправильных ответов:%d",arrayAns["right"],arrayAns["incorrect"])
+
+	fmt.Println("Правильных ответов:" + strconv.Itoa(arrayAns["right"]) + " Неправильных ответов:" + strconv.Itoa(arrayAns["incorrect"]))
 }
